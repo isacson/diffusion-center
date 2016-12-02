52f5db8d106749f5fee34eb015026afe4a2d0c12
isacson <thisisadamsmail@gmail.com>
2016-12-02 03:47:37 -0500

    Add files via upload
    Added files necessary for secure login when adding blog posts.

---
+++ psl-config_example.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "whatever");    // The database username. 
+define("PASSWORD", "whatever");    // The database password. 
+define("DATABASE", "blog");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
